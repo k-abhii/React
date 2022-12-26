aa29edbadae4389c449f9f29cd350bf963a4fd17
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2022-12-26 19:47:39 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ function App() {
       <section className="container">
         <h3>{people.length} birthday today</h3>
         <List people={people} />
-        <button onClick={() => console.log("you clicked me ")}>
-          Clear All
-        </button>
+        <button onClick={() => setPeople([])}>Clear All</button>
       </section>
     </main>
   );
